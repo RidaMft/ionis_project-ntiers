7b5f6b7d28c1ca8b0d8fc58e540bf2ac78d7b057
RidaMft <rida.meftah@gmail.com>
2017-03-29 01:47:54 +0200

    pour l’indentation

--- index.php
+++ index.php
@@ -20,150 +20,149 @@
 
             switch($uc)
             {
-                    case 'accueil':
-                    {
-                        include("vues/v_bandeau.php");
-                        include("vues/v_accueil.php");
-                        break;            
+                case 'accueil':
+                {
+                    include("vues/v_bandeau.php");
+                    include("vues/v_accueil.php");
+                    break;            
+                }
+
+                case 'voirProduits' :
+                    include("vues/v_bandeau.php");
+                    include("vues/v_afficheProduits.php");  
+                    break;
+
+                case 'administrer' :
+                    include("vues/v_bandeau.php") ;
+                    if(!isset($_SESSION['login'])&& (!isset($_SESSION['passwd'])))
+                    {        
+                        include("vues/v_connexion.php");  
                     }
+                    else
+                    {
+                        include("vues/v_voirProduits.php");  
+                    }
+                    break;
 
-                    case 'voirProduits' :
-                        include("vues/v_bandeau.php");
-                        include("vues/v_afficheProduits.php");  
-                        break;
+                case  'deconnexion':
+                    include("vues/v_bandeau.php") ;
+                    session_unset();
+                    echo 'Déconnexion réussie';
+                    break;
 
-                    case 'administrer' :
-                        include("vues/v_bandeau.php") ;
-                        if(!isset($_SESSION['login'])&& (!isset($_SESSION['passwd'])))
-                        {        
-                            include("vues/v_connexion.php");  
-                        }
-                        else
-                        {
-                            include("vues/v_voirProduits.php");  
-                        }
-                        break;
+                case 'modifier':
+                    include("vues/v_bandeauAdmin.php") ;
+                    $id=$_GET['cleP'];
+                    $unProduit=$pdo->afficheProduit($id); 
+                    include 'vues/v_modif.php';
+                    break;
 
-                    case  'deconnexion':
-                        include("vues/v_bandeau.php") ;
-                        session_unset();
-                        echo 'Déconnexion réussie';
-                        break;
+                case 'modification':
+                    include("vues/v_bandeauAdmin.php") ;
+                    $id=$_REQUEST['id'];
+                    $libelle=$_POST['libelle'];
+                    $prix=$_POST['prix'];
+                    $description=$_POST['description'];
+                    $tva=$_POST['tva'];
+                    $photo=$_POST['photo'];
 
-                    case 'modifier':
-                        include("vues/v_bandeauAdmin.php") ;
-                        $id=$_GET['cleP'];
-                        $unProduit=$pdo->afficheProduit($id); 
-                        include 'vues/v_modif.php';
-                        break;
+                    $res=$pdo->modifProduit($id, $libelle, $prix, $description, $tva, $photo);
+                    
+                    if($res)
+                    {
+                        echo 'Modification pris en compte'; 
+                        include("vues/v_voirProduits.php");  
+                    }
+                    else
+                    {
+                        echo 'Erreur modification';
+                    }
+                    break;
 
-                    case 'modification':
-                        include("vues/v_bandeauAdmin.php") ;
+                case 'suppression':
+                    include("vues/v_bandeauAdmin.php") ;
+                    $id=$_REQUEST['cleP'];
+                    $res=$pdo->suppProduit($id);
+                    if($res)
+                    {
+                        echo 'Suppression effectué';
+                        include("vues/v_voirProduits.php");  
+                    }
+                    else 
+                    {
+                        echo 'Erreur suppression';
+                    }
+                    break;
+
+                case 'ajouter':
+                    include("vues/v_bandeauAdmin.php") ;
+                    include 'vues/v_ajout.php';
+                    break;
+
+                case 'insertion':
+                    include("vues/v_bandeauAdmin.php") ;
+                    if(!isset($_REQUEST['ajouter']))
+                    {
                         $id=$_REQUEST['id'];
                         $libelle=$_POST['libelle'];
                         $prix=$_POST['prix'];
+                        $quantite = $_POST['quantite'];
                         $description=$_POST['description'];
                         $tva=$_POST['tva'];
                         $photo=$_POST['photo'];
+                        $res=$pdo->ajoutProduit($id,$libelle,$prix, $quantite, $description, $tva, $photo);
+                        include("vues/v_voirProduits.php");                        
+                    }
+                    break;
 
-                        $res=$pdo->modifProduit($id, $libelle, $prix, $description, $tva, $photo);
-                        
-                        if($res)
-                        {
-                            echo 'Modification pris en compte'; 
-                            include("vues/v_voirProduits.php");  
-                        }
-                        else
-                        {
-                            echo 'Erreur ';
-                        }
-                        break;
+                case 'connexion' :
+                    include("vues/v_bandeauAdmin.php") ;
+                    $login=$_POST['login'];
+                    $passwd=$_POST['passwd'];
 
-                    case 'suppression':
-                        include("vues/v_bandeauAdmin.php") ;
-                        $id=$_REQUEST['cleP'];
-                        $res=$pdo->suppProduit($id);
-                        if($res)
-                        {
-                            echo 'Suppression effectué';
-                            include("vues/v_voirProduits.php");  
-                        }
-                        else 
-                        {
-                            echo 'Erreur';
-                        }
-                        break;
-
-                    case 'ajouter':
-                        include("vues/v_bandeauAdmin.php") ;
-                        include 'vues/v_ajout.php';
-                        break;
-
-                    case 'insertion':
-                        include("vues/v_bandeauAdmin.php") ;
-                        if(!isset($_REQUEST['ajouter']))
-                        {
-                            $id=$_REQUEST['id'];
-                            $libelle=$_POST['libelle'];
-                            $prix=$_POST['prix'];
-                            $quantite = $_POST['quantite'];
-                            $description=$_POST['description'];
-                            $tva=$_POST['tva'];
-                            $photo=$_POST['photo'];
-                            $res=$pdo->ajoutProduit($id,$libelle,$prix, $quantite, $description, $tva, $photo);
-                            include("vues/v_voirProduits.php");                        
-                        }
-                        break;
-
-                    case 'connexion' :
-                        include("vues/v_bandeauAdmin.php") ;
-                        $login=$_POST['login'];
-                        $passwd=$_POST['passwd'];
-
-                        $res=$pdo->log($login,$passwd);
-                        if ($res == 0)
-                        {
-                              include("vues/v_erreur.php");                   
-                        }
-                        else
-                        {
-                            $_SESSION['login']=$login;
-                            $_SESSION['passwd']=$passwd;
-                              include("vues/v_voirProduits.php");                         
-                        }                      
-                        break;
-                        
-                    case 'login_espace':
-                        include("vues/v_bandeau.php") ;
-                        if(!isset($_SESSION['login'])&& (!isset($_SESSION['passwd'])))
-                        {        
-                            include("vues/v_connexionEspace.php");  
-                        }
-                        else
-                        {
-                            include("vues/v_voirEspace.php");  
-                        }
-                        break;
-                        break;
+                    $res=$pdo->log($login,$passwd);
+                    if ($res == 0)
+                    {
+                          include("vues/v_erreur.php");                   
+                    }
+                    else
+                    {
+                        $_SESSION['login']=$login;
+                        $_SESSION['passwd']=$passwd;
+                          include("vues/v_voirProduits.php");                         
+                    }                      
+                    break;
                     
-                    case 'connexion_espace':
-                        include("vues/v_bandeau.php") ;
-                        $login=$_POST['login'];
-                        $mdp=$_POST['passwd'];
+                case 'login_espace':
+                    include("vues/v_bandeau.php") ;
+                    if(!isset($_SESSION['login'])&& (!isset($_SESSION['passwd'])))
+                    {        
+                        include("vues/v_connexionEspace.php");  
+                    }
+                    else
+                    {
+                        include("vues/v_voirEspace.php");  
+                    }
+                    break;
+                    break;
+                
+                case 'connexion_espace':
+                    include("vues/v_bandeau.php") ;
+                    $login=$_POST['login'];
+                    $mdp=$_POST['passwd'];
 
-                        $res=$pdo->loginUtilisateur($login,$mdp);
-                        if ($res == 0)
-                        {
-                              include("vues/v_erreur.php");                   
-                        }
-                        else
-                        {
-                            $_SESSION['login']=$login;
-                            $_SESSION['passwd']=$mdp;
-                            include("vues/v_voirEspace.php"); 
-                        }                      
-                        break;
-                                
+                    $res=$pdo->loginUtilisateur($login,$mdp);
+                    if ($res == 0)
+                    {
+                          include("vues/v_erreur.php");                   
+                    }
+                    else
+                    {
+                        $_SESSION['login']=$login;
+                        $_SESSION['passwd']=$mdp;
+                        include("vues/v_voirEspace.php"); 
+                    }                      
+                    break;  
             }
             include("vues/v_pied.php");
             ?>
--- util/class.PDO.Ionis.inc.php
+++ util/class.PDO.Ionis.inc.php
@@ -1,10 +1,10 @@
 <?php
 class PdoIonis
 {   		
-      	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=archi_ntiers';   		
-      	private static $user='root' ;    		
-      	private static $mdp='root' ;	
+  	private static $serveur='mysql:host=localhost';
+  	private static $bdd='dbname=archi_ntiers';   		
+  	private static $user='root' ;    		
+  	private static $mdp='root' ;	
 		private static $monPdo;
 		private static $monPdoIonis = null;
 /**
@@ -13,7 +13,7 @@ class PdoIonis
  */				
 	private function __construct()
 	{
-    		PdoIonis::$monPdo = new PDO(PdoIonis::$serveur.';'.PdoIonis::$bdd, PdoIonis::$user, PdoIonis::$mdp); 
+  		PdoIonis::$monPdo = new PDO(PdoIonis::$serveur.';'.PdoIonis::$bdd, PdoIonis::$user, PdoIonis::$mdp); 
 			PdoIonis::$monPdo->query("SET CHARACTER SET utf8");
 	}
 	public function _destruct(){
@@ -25,6 +25,7 @@ class PdoIonis
  * Appel : $instancePdoIonis = PdoIonis::getPdoIonis();
  * @return l'unique objet de la classe PdoIonis
  */
+
 	public  static function getPdoIonis()
 	{
 		if(PdoIonis::$monPdoIonis == null)
@@ -48,93 +49,94 @@ class PdoIonis
 		$lesProduits=array();
 		if($nbProduits != 0)
 		{
-                    foreach($lesid as $id)
-                    {
-                        $req = "select * from produit where id = '$id'";
-                        $res=  PdoIonis::$monPdo->query($req);
-                        $unProduit = $res->fetch();
-                        $lesProduits[] = $unProduit;
-                    }
+      foreach($lesid as $id)
+      {
+          $req = "select * from produit where id = '$id'";
+          $res=  PdoIonis::$monPdo->query($req);
+          $unProduit = $res->fetch();
+          $lesProduits[] = $unProduit;
+      }
 		}
 		return $lesProduits;
 	}
-        
-       
-        public function ajoutProduit($id,$libelle, $prix, $quantite, $description, $tva, $photo)
-        {
-              $sql = "INSERT  INTO produit (libelle, prix, quantite, description, tva, photo)
-                        VALUES ('$libelle', $prix, $quantite, '$description', '$tva','$photo')";
+              
+  public function ajoutProduit($id,$libelle, $prix, $quantite, $description, $tva, $photo)
+  {
+    $sql = "INSERT  INTO produit (libelle, prix, quantite, description, tva, photo)
+              VALUES ('$libelle', $prix, $quantite, '$description', '$tva','$photo')";
 
-              $res = PdoIonis::$monPdo->query($sql);
-              echo $sql;
-              //affichage des résultats, pour savoir si l'insertion a marchée:
-              if($res)
-              {
-                  echo'<html><body><br>';
-                  echo("L'insertion a été correctement effectuée") ;
-                  echo'<br></body></html>';
-              }
-              else
-              {
-                echo("L'insertion a  échoué") ;
-              }
-              return $res;
-        }
-        public function modifProduit($id,$libelle, $prix, $quantite, $description, $tva, $photo)
-        {
-            $req = "UPDATE produit SET 
-                    id = '$id',
-                    libelle = '$libelle',
-                    quantite = $quantite ,
-                    prix = $prix,
-                    description = '$description',
-                    tva = '$tva',
-                    photo = '$photo',
-                    WHERE id = '$id'" ;
-            $res=PdoIonis::$monPdo->query($req);
-            echo $req;
-            return $res;
-        }
-        
-        public function afficheProduit($id)
-        {
-            $req = "select * from produit where id = '$id'";
-            $res =  PdoIonis::$monPdo->query($req);
-            $unProduit = $res->fetch();
-            return $unProduit;          
-        }
-        
-        public function suppProduit($id)
-        {
-            $req = "delete from produit where id ='$id'";
-            $res =  PdoIonis::$monPdo->query($req);
-            return $res;            
-        }
+    $res = PdoIonis::$monPdo->query($sql);
+    echo $sql;
+    //affichage des résultats, pour savoir si l'insertion a marchée:
+    if($res)
+    {
+        echo'<html><body><br>';
+        echo("L'insertion a été correctement effectuée") ;
+        echo'<br></body></html>';
+    }
+    else
+    {
+      echo("L'insertion a  échoué") ;
+    }
+    return $res;
+  }
 
-        public function log($login,$passwd)
-        {
-            $req = "select count(*) as nb from utilisateur where login = '$login' and mdp = '$passwd'";
-            $res =  PdoIonis::$monPdo->query($req);
-            $ligne = $res->fetch();
-            $nb=$ligne['nb'];
-            return $nb;    
-        }
+  public function modifProduit($id,$libelle, $prix, $quantite, $description, $tva, $photo)
+  {
+      $req = "UPDATE produit SET 
+              id = '$id',
+              libelle = '$libelle',
+              quantite = $quantite ,
+              prix = $prix,
+              description = '$description',
+              tva = '$tva',
+              photo = '$photo',
+              WHERE id = '$id'" ;
+
+      $res=PdoIonis::$monPdo->query($req);
+      return $res;
+  }
         
-        public function loginEspace($login,$mdp)
-        {
-            $req = "select count(*) as nb from utilisateur where login = '$login' and mdp = '$mdp'";
-            $res =  PdoIonis::$monPdo->query($req);
-            $ligne = $res->fetch();
-            $nb = $ligne['nb'];
-            return $nb;    
-        }
+  public function afficheProduit($id)
+  {
+      $req = "select * from produit where id = '$id'";
+      $res =  PdoIonis::$monPdo->query($req);
+      $unProduit = $res->fetch();
+      return $unProduit;          
+  }
+  
+  public function suppProduit($id)
+  {
+      $req = "delete from produit where id ='$id'";
+      $res =  PdoIonis::$monPdo->query($req);
+      return $res;            
+  }
+
+  public function log($login,$passwd)
+  {
+      $req = "select count(*) as nb from utilisateur where login = '$login' and mdp = '$passwd'";
+      $res =  PdoIonis::$monPdo->query($req);
+      $ligne = $res->fetch();
+      $nb=$ligne['nb'];
+      return $nb;    
+  }
+  
+  public function loginEspace($login,$mdp)
+  {
+      $req = "select count(*) as nb from utilisateur where login = '$login' and mdp = '$mdp'";
+      $res =  PdoIonis::$monPdo->query($req);
+      $ligne = $res->fetch();
+      $nb = $ligne['nb'];
+      return $nb;    
+  }
         
-        public function getUnUtilisateur($login)
-	{
-            $req = "select * from utilisateur where login ='$login";
-            $res =  PdoIonis::$monPdo->query($req);
-            $lesLignes = $res->fetchAll();
-            return $lesLignes;
+  public function getUnUtilisateur($login)
+  {
+    $req = "select * from utilisateur where login ='$login";
+    $res =  PdoIonis::$monPdo->query($req);
+    $lesLignes = $res->fetchAll();
+    return $lesLignes;
 	}
 }
+
 ?>
